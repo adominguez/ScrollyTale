4f2c68895d702dfc65a802140c2c67167f080121
Adominguez <dmartin.alberto@gmail.com>
2026-07-07 17:05:11 +0200

    Añadir soporte para informes de cobertura de pruebas y actualizar configuración de Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { getViteConfig } from 'astro/config';
 export default getViteConfig({
   test: {
     include: ['test/**/*.test.{js,ts}'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{js,astro}'],
+      reporter: ['text', 'html'],
+    },
   },
 });
